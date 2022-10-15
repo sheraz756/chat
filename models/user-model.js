abd202dc53de57c4eced749d0f1ee37e36dd7d67
sheraz756 <106003519+sheraz756@users.noreply.github.com>
2022-10-16 02:16:07 +0500

    db

--- models/user-model.js
+++ models/user-model.js
@@ -10,7 +10,7 @@ const userSchema = new Schema(
             required: false,
             get: (avatar) => {
                 if (avatar) {
-                    return `/images/monkey-avatar.png`;
+                    return `${process.env.BASE_URL}${avatar}`;
                 }
                 return avatar;
             },
