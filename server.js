b2685627bd787abdc79b011cb4162bc3f4faecd4
sheraz756 <106003519+sheraz756@users.noreply.github.com>
2022-10-15 21:37:43 +0500

    ok

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json({ limit: '8mb' }));
 app.use(router);
 
 app.get('/', (req, res) => {
-    res.send('Hello from express Js');
+    res.send('Hello from express');
 });
 
 // Sockets
