e11a3d740057930fac1afe9ec25d7e1750db21b7
sheraz756 <106003519+sheraz756@users.noreply.github.com>
2022-10-17 03:36:46 +0500

    ok

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -3,6 +3,7 @@ const tokenService = require('../services/token-service');
 module.exports = async function (req, res, next) {
     try {
         const { accessToken } = req.cookies;
+        console.log('access token',accessToken)
         if (!accessToken) {
             throw new Error();
         }
@@ -13,6 +14,6 @@ module.exports = async function (req, res, next) {
         req.user = userData;
         next();
     } catch (err) {
-        res.status(401).json({ message: 'Invalid token' });
+        res.status(401).json({ message: 'Invalid token',cookies:req.cookies });
     }
 };
