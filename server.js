9fff2bbb3aba3a1c3789fad5633dcf8df7945dcb
sheraz756 <106003519+sheraz756@users.noreply.github.com>
2022-10-16 07:54:07 +0500

    ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const ACTIONS = require('./actions');
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'http://anonytesting-cf.preview-domain.com',
+        origin: 'http://anonytesting-cf.preview-domain.com ,http://localhost:3000',
         methods: ['GET', 'POST'],
     },
 });
@@ -22,7 +22,7 @@ app.get('/',(req,res)=>{
 app.use(cookieParser());
 const corsOption = {
     credentials: true,
-    origin: 'http://anonytesting-cf.preview-domain.com',
+    origin: 'http://anonytesting-cf.preview-domain.com ,http://localhost:3000',
 };
 app.use(cors(corsOption));
 app.use('/storage', express.static('storage'));
