bea77bfef36f6a140d5377013cb799f15ff9fe20
sheraz756 <106003519+sheraz756@users.noreply.github.com>
2022-10-17 15:03:13 +0500

    ok

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const roomsController = require('./controllers/rooms-controller');
 
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
-router.get('/api/activate', authMiddleware, activateController.activate);
+router.post('/api/activate', authMiddleware, activateController.activate);
 router.get('/api/refresh', authController.refresh);
 router.post('/api/logout', authMiddleware, authController.logout);
 router.post('/api/rooms', authMiddleware, roomsController.create);
