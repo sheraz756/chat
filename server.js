08111ed43602851a85457a3a1b48695109759a65
sheraz756 <106003519+sheraz756@users.noreply.github.com>
2022-10-17 21:38:26 +0500

    ok

--- server.js
+++ server.js
@@ -1,136 +1,133 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const DbConnect = require('./database');
-const router = require('./routes');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const env = require('dotenv')
+const server = require("http").createServer(app);
+const DbConnect = require("./database");
+const router = require("./routes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const env = require("dotenv");
 
-const ACTIONS = require('./actions');
+const ACTIONS = require("./actions");
 
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-    },
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://anonytesting-cf.preview-domain.com",
+    credentials: true,
+    methods: ["GET", "POST"],
+    origin: "*",
+  },
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname,'public')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "public");
+});
 app.use(cookieParser());
-const corsOption = {
-    credentials: true,
-    origin: 'http://anonytesting-cf.preview-domain.com',
-};
-app.use(cors(corsOption));
-app.use('/storage', express.static('storage'));
+const corsOption = {};
+app.use(cors());
+app.use("/storage", express.static("storage"));
 
 const PORT = process.env.PORT || 5500;
 DbConnect();
-app.use(express.json({ limit: '8mb' }));
+app.use(express.json({ limit: "8mb" }));
 app.use(router);
 
-app.get('/', (req, res) => {
-    res.send('Hello from express');
+app.get("/", (req, res) => {
+  res.send("Hello from express");
 });
 
 // Sockets
 const socketUserMap = {};
 
-io.on('connection', (socket) => {
-    console.log('New connection', socket.id);
-    socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
-        socketUserMap[socket.id] = user;
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.ADD_PEER, {
-                peerId: socket.id,
-                createOffer: false,
-                user,
-            });
-            socket.emit(ACTIONS.ADD_PEER, {
-                peerId: clientId,
-                createOffer: true,
-                user: socketUserMap[clientId],
-            });
-        });
-        socket.join(roomId);
+io.on("connection", (socket) => {
+  console.log("New connection", socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
+    socketUserMap[socket.id] = user;
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {
+        peerId: socket.id,
+        createOffer: false,
+        user,
+      });
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerId: clientId,
+        createOffer: true,
+        user: socketUserMap[clientId],
+      });
     });
+    socket.join(roomId);
+  });
 
-    socket.on(ACTIONS.RELAY_ICE, ({ peerId, icecandidate }) => {
-        io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
-            peerId: socket.id,
-            icecandidate,
-        });
+  socket.on(ACTIONS.RELAY_ICE, ({ peerId, icecandidate }) => {
+    io.to(peerId).emit(ACTIONS.ICE_CANDIDATE, {
+      peerId: socket.id,
+      icecandidate,
     });
+  });
 
-    socket.on(ACTIONS.RELAY_SDP, ({ peerId, sessionDescription }) => {
-        io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
-            peerId: socket.id,
-            sessionDescription,
-        });
+  socket.on(ACTIONS.RELAY_SDP, ({ peerId, sessionDescription }) => {
+    io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerId: socket.id,
+      sessionDescription,
     });
+  });
 
-    socket.on(ACTIONS.MUTE, ({ roomId, userId }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.MUTE, {
-                peerId: socket.id,
-                userId,
-            });
-        });
+  socket.on(ACTIONS.MUTE, ({ roomId, userId }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.MUTE, {
+        peerId: socket.id,
+        userId,
+      });
     });
+  });
 
-    socket.on(ACTIONS.UNMUTE, ({ roomId, userId }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            io.to(clientId).emit(ACTIONS.UNMUTE, {
-                peerId: socket.id,
-                userId,
-            });
-        });
+  socket.on(ACTIONS.UNMUTE, ({ roomId, userId }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.UNMUTE, {
+        peerId: socket.id,
+        userId,
+      });
     });
+  });
 
-    socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
-        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
-        clients.forEach((clientId) => {
-            if (clientId !== socket.id) {
-                console.log('mute info');
-                io.to(clientId).emit(ACTIONS.MUTE_INFO, {
-                    userId,
-                    isMute,
-                });
-            }
+  socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      if (clientId !== socket.id) {
+        console.log("mute info");
+        io.to(clientId).emit(ACTIONS.MUTE_INFO, {
+          userId,
+          isMute,
         });
+      }
     });
+  });
 
-    const leaveRoom = () => {
-        const { rooms } = socket;
-        Array.from(rooms).forEach((roomId) => {
-            const clients = Array.from(
-                io.sockets.adapter.rooms.get(roomId) || []
-            );
-            clients.forEach((clientId) => {
-                io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
-                    peerId: socket.id,
-                    userId: socketUserMap[socket.id]?.id,
-                });
-
-                // socket.emit(ACTIONS.REMOVE_PEER, {
-                //     peerId: clientId,
-                //     userId: socketUserMap[clientId]?.id,
-                // });
-            });
-            socket.leave(roomId);
+  const leaveRoom = () => {
+    const { rooms } = socket;
+    Array.from(rooms).forEach((roomId) => {
+      const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+      clients.forEach((clientId) => {
+        io.to(clientId).emit(ACTIONS.REMOVE_PEER, {
+          peerId: socket.id,
+          userId: socketUserMap[socket.id]?.id,
         });
-        delete socketUserMap[socket.id];
-    };
 
-    socket.on(ACTIONS.LEAVE, leaveRoom);
+        // socket.emit(ACTIONS.REMOVE_PEER, {
+        //     peerId: clientId,
+        //     userId: socketUserMap[clientId]?.id,
+        // });
+      });
+      socket.leave(roomId);
+    });
+    delete socketUserMap[socket.id];
+  };
 
-    socket.on('disconnecting', leaveRoom);
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+
+  socket.on("disconnecting", leaveRoom);
 });
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
